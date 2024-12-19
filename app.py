972304d9ef74b175efca174ff6ab3f6e663a2264
andriszeile <71541542+andriszeile@users.noreply.github.com>
2024-12-19 17:36:02 +0200

    12

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import json
 import os
 
-app = Flask(__name__)   #bug Flask ar lielo
+app = Flask(__name__)
 DATA_FILE = "data.json"
 
 #JSON faila ielāde un inicalizācija
